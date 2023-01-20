a4a913b6e36e32624e84d4d5a315fac30e771925
Antony Bartolomucci <antony@passbolt.com>
2023-01-20 11:07:59 +0100

    add instructions about references types on objects and array

---
+++ JavaScript-QuickRefresher/arrayAndObjectsReferenceTypes.js
@@ -0,0 +1,6 @@
+const names = ['Antony', 'Thomas', 'Romain', 'Baptiste']
+
+// Normalement, comme vu dans le fichier ./coreSyntax il n'est pas possible de réattribuer des valeurs dans des constantes. Mais avec les tableaux cela est différent, je peux utiliser la méthode push qui va ajouter un élément dans mon tableau. Car la méthode ne va pas changer "l'addresse" de la variable il va seulement ajouter une nouvelle valeur. On ne va pas redéclarer cette variable. 
+
+names.push('Jordan')
+console.log(names)
