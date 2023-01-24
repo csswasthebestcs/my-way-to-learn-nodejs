935ca880974d451f40ba638fa1c502e0e1131b42
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-01-24 06:57:08 +0100

    add explainations about backquotes to do template litterals

---
+++ JavaScript-QuickRefresher/templateLiterals.js
@@ -0,0 +1,9 @@
+// Il y'a une autre méthode pour afficher du texte qui est différent de ces deux variables
+let singleQuote = 'A string'
+let doubleQuote = "A second string"
+
+// Il y'a le back-quotes qui va nous permettre d'ajouter dynamiquement des données dans une chaîne de caractères 
+let backQuotesName = 'Antony'
+let backQuotesAge = 25
+
+console.log(`My name is ${backQuotesName} and I am ${backQuotesAge} years old`)
